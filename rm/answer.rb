15d803a87cea61cf24eabffe6d612bce618f2dd7
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-07-17 22:04:15 +0900

    Fix rm

--- rm/answer.rb
+++ rm/answer.rb
@@ -2,22 +2,22 @@ require 'bigdecimal'
 require 'bigdecimal/util'
 
 def calc_max_weight(weight, count)
-  (weight * (1 + (count.to_d / 40.0))).round(1).to_f
+  (weight * (1 + (count.to_d / 40.0))).round(1)
+end
+
+def print_num_x_unit(num, unit)
+  print("#{num}#{unit}".to_s.rjust(8))
+  print('|')
 end
 
 print('|')
-1.upto(10) do |count|
-  print("#{count}reps".to_s.rjust(8))
-  print('|')
-end
+1.upto(12) { |count| print_num_x_unit(count, 'reps') }
 print("\n")
 
 40.step(150, 5) do |weight|
   print('|')
-  1.upto(10) do |count|
-    print("#{calc_max_weight(weight, count)}kg".rjust(8))
-    print('|')
-  end
+  print_num_x_unit(weight.to_f, 'kg')
+  2.upto(12) { |count| print_num_x_unit(calc_max_weight(weight, count).to_f, 'kg') }
   print("\n")
 end
 
