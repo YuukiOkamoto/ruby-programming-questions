9f90042509b1710db335efba394728a5c12835da
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-07-18 12:22:06 +0900

    Change name

--- rm/answer.rb
+++ rm/answer.rb
@@ -4,18 +4,18 @@ def calc_max_weight(weight, count)
   (weight * (1 + (BigDecimal(count) / 40))).round(1)
 end
 
-def print_num_x_unit(num, unit)
+def print_cell(num, unit)
   print("#{num}#{unit}".to_s.rjust(8))
   print('|')
 end
 
 print('|')
-1.upto(12) { |count| print_num_x_unit(count, 'reps') }
+1.upto(12) { |count| print_cell(count, 'reps') }
 print("\n")
 
 40.step(150, 5) do |weight|
   print('|')
-  print_num_x_unit(weight.to_f, 'kg')
-  2.upto(12) { |count| print_num_x_unit(calc_max_weight(weight, count).to_f, 'kg') }
+  print_cell(weight.to_f, 'kg')
+  2.upto(12) { |count| print_cell(calc_max_weight(weight, count).to_f, 'kg') }
   print("\n")
 end
